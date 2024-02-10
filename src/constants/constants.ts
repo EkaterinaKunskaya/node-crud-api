997645ebf6e0702fb5a24112ecf770012560d05a
EkaterinaKunskaya <kunskaya.ekaterina@gmail.com>
2024-02-10 23:59:57 +0300

    feat: add constants

---
+++ src/constants/constants.ts
@@ -0,0 +1,13 @@
+const API_BASE_PATH = '/api/users';
+
+const METHOD = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+};
+
+export {
+    API_BASE_PATH,
+    METHOD,
+}
